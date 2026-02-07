4b7788816f20fa00a0e1c3507055d68498c6403d
Muhammad Hunain <mhunain8007@gmail.com>
2026-02-07 04:01:03 -0800

    Add files via upload

---
+++ video/simple_capture.py
@@ -0,0 +1,17 @@
+import cv2 
+
+cap=cv2.VideoCapture(0)
+
+while True:
+    res , frame = cap.read()
+    if not res:
+        print("Failed to grab the frames")
+        break
+    cv2.imshow("webcam feed", frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        print("quitting ......")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
